66f96af16c1a47be27ab1fcb7c24307ccdd32905
Miracle <chat.miracle@gmail.com>
2024-07-06 15:59:54 -0400

    added logic for scoring

--- javascript.js
+++ javascript.js
@@ -19,28 +19,27 @@ let computerScore = 0
 function playRound(humanChoice, computerChoice){
     let humanInput = humanChoice.toLowerCase()
     if (humanInput == "rock" && computerChoice == "paper") {
-        let roundWinner = "compuer"
+        computerScore++
         console.log("You lose! paper beats rock")
     } else if (humanInput == "rock" && computerChoice == "sissors") {
-        let roundWinner = "human"
+        humanScore++
         console.log("You win! rock beats sissors")
     }  else if (humanInput == "paper" && computerChoice == "sissors") {
-        let roundWinner = "computer"
+        computerScore++
         console.log("You win! sissors beats paper")
     } else if (humanInput == "paper" && computerChoice == "rock") {
-        let roundWinner = "human"
+        humanScore++
         console.log("You win! paper beats rock")
     } else if (humanInput == "sissors" && computerChoice == "paper") {
-        let roundWinner = "human"
+        humanScore++
         console.log("You win! sissors beats paper")
     } else if (humanInput == "sissors" && computerChoice == "rock") {
-        let roundWinner = "computer"
+        computerScore++
         console.log("You lose! rock beats sissors")
     } else if (humanInput == computerChoice ){
-        let roundWinner = "draw"
         console.log("It's a draw! you both chose the same thing")
     }
-
 }
+
 
 playRound(getHumanChoice(),getComputerChoice())
